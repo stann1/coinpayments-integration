ccc91f4d9d23102baa9266b3b8e9d6d8e0ed214b
Stanislav Stoychev <stoychev.st2@gmail.com>
2022-03-18 18:48:45 +0200

    initial push

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using coinpayments_test.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+
+namespace coinpayments_test.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
+
+  public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+  {
+    _logger = logger;
+    _configuration = configuration;
+  }
+
+  public IActionResult Index()
+    {
+        ViewData["ClientUrl"] = _configuration["ClientUrl"];
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpPost]
+    public IActionResult Webhook()
+    {
+        _logger.LogInformation($"Received payload:");
+        // _logger.LogInformation(payload.ToString());
+        var dictionary = HttpContext.Request.Form.Keys.ToDictionary(s => s, s => HttpContext.Request.Form[s]);
+        string json = JsonConvert.SerializeObject(dictionary, Formatting.Indented);
+        _logger.LogInformation(json);
+        
+        // foreach(var key in HttpContext.Request.Form.Keys)
+        // {
+        //     var val = HttpContext.Request.Form[key];
+        //     _logger.LogInformation($"{key}: {val}");
+
+        //     //process the form data
+        // }
+
+        return Ok();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddLogging(opt => 
+{
+    opt.AddSimpleConsole(options =>
+                {
+                    options.IncludeScopes = true;
+                    options.SingleLine = true;
+                    options.TimestampFormat = "hh:mm:ss ";
+                });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
